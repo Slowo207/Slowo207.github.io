19c1a77e75f94c337db55e569ceab6b092c55b71
M_E <superpowermachine@gmail.com>
2022-07-27 15:44:26 +0800

    modified:   sketch.js

--- server/games/game3/sketch.js
+++ server/games/game3/sketch.js
@@ -5,8 +5,16 @@ function setup() {
   fish = 0
   cost = 0
   change = 0 
-  state = 0
+  gameState = 0
   score = 0
+  level = 4
+  levelArray = [
+    [0.1, 1, 0.1, false],
+    [0.1, 1, 0.05, false],
+    [1, 100, 10, true],
+    [1, 100, 5, true],
+    [1, 100, 1, true]
+  ]
   
   registerArray.push(new Register(100+75*0, 400, 75, 200, 50))
   registerArray.push(new Register(100+75*1, 400, 75, 200, 20))
@@ -18,16 +26,42 @@ function setup() {
   registerArray.push(new Register(100+75*2, 600, 75, 100, 0.25))
   registerArray.push(new Register(100+75*3, 600, 75, 100, 0.10))
   
-  for(i=0; i < 10; i++){
-    cost = getRndInteger(50, 200)
-    fishArray.push(new Fish(cost, roundUpNearest10(getRndInteger(cost, cost + 20))))
-  }  
   //registerArray.push()
   
+  fishPrice(levelArray[level])
+}
+
+function fishPrice(priceArray){
+  startPrice = priceArray[0]
+  endPrice = priceArray[1]
+  increment = priceArray[2]
+  rD = priceArray[3]
+  
+  for(i=0; i < 10; i++){
+    cost = getRndInteger(startPrice, endPrice, rD)
+    fishArray.push(new Fish(cost, findPaid(cost,endPrice, increment)))
+  }  
 }
 
 function draw() {
   background(220);
+  
+  if(gameState == 0){
+    startPage()
+  }else if (gameState == 1){
+    gamePage()
+  }
+}
+
+function startPage(){
+  textSize(32)
+  fill("black")
+  text("Start", 370, 350)
+  noFill()
+  rect(300, 300, 200, 100)
+}
+
+function gamePage(){
   textSize(32)
   fill("black")
   text(score, 50, 50)
@@ -69,25 +103,44 @@ function draw() {
 }
 
 function mouseClicked(){
-  if(mouseX < 400 && mouseY > 400 ){
-    for(i=0; i < registerArray.length; i++){
-      change += registerArray[i].clicked()
+  if(gameState == 0){
+    if((mouseX > 300 && mouseX < 500) && (mouseY > 300 && mouseY < 400)){
+      gameState = 1
     }
-  }else if((mouseX < 350 && mouseX > 150)&&(mouseY < 380 && mouseY > 350)){
-    if(change == fishArray[fish].change){
-      score += 1
-    }else{
-      console.log(fishArray[fish].change, change)
+  }else if (gameState == 1){
+    if(mouseX < 400 && mouseY > 400 ){
+      for(i=0; i < registerArray.length; i++){
+        change += registerArray[i].clicked()
+      }
+    }else if((mouseX < 350 && mouseX > 150)&&(mouseY < 380 && mouseY > 350)){
+      if(change == fishArray[fish].change){
+        score += 1
+      }else{
+        console.log(fishArray[fish].change, change)
+      }
+      change = 0
+      fish += 1
     }
-    change = 0
-    fish += 1
   }
 }
 
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
+function getRndInteger(min, max, rD) {
+  n = (Math.random() * (max - min)  + min)*100;
+  n = Math.floor(n)/100
+  if(rD == true){
+    return Math.floor(n)
+  }else if(rD == false){
+    return n
+  }
 }
 
-function roundUpNearest10(num) {
-  return Math.ceil(num/10)*10
+function removeDecimal(n){
+  return Math.floor(n)
+}
+
+function findPaid(num, endPrice, increment) {
+  m = endPrice - num
+  mRemainder = Math.floor(m/increment)
+  mRandom = getRndInteger(1, mRemainder, true)
+  return num + increment*mRandom
 }
