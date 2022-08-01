a47019c3e7a50fac41f7813a3d3b06bb25ebf256
M_E <superpowermachine@gmail.com>
2022-08-01 14:21:53 +0800

    modified:   games/game3/fish.js 	modified:   games/game3/sketch.js 	deleted:    games/game3/Assets/Boy.png 	deleted:    games/game3/Assets/BrassCoin.png 	deleted:    games/game3/Assets/Fish_blue.png 	deleted:    games/game3/Assets/Fish_green.png 	deleted:    games/game3/Assets/Fish_orange.png 	deleted:    games/game3/Assets/Fish_purple.png 	deleted:    games/game3/Assets/Fish_yellow.png 	deleted:    games/game3/Assets/Girl.png 	deleted:    games/game3/Assets/GoldCoin.png 	deleted:    games/game3/Assets/Grandma.png 	deleted:    games/game3/Assets/Item_blue.png 	deleted:    games/game3/Assets/Item_green.png 	deleted:    games/game3/Assets/Item_orange.png 	deleted:    games/game3/Assets/Item_purple.png 	deleted:    games/game3/Assets/Item_yellow.png 	deleted:    games/game3/Assets/Note10.png 	deleted:    games/game3/Assets/Note20.png 	deleted:    games/game3/Assets/Note5.png 	deleted:    games/game3/Assets/Note50.png 	deleted:    games/game3/Assets/PriceTag.png 	deleted:    games/game3/Assets/Register.png 	deleted:    games/game3/Assets/SilverCoin.png

--- server/games/game3/fish.js
+++ server/games/game3/fish.js
@@ -1,5 +1,5 @@
 class Fish{
-  constructor(cost, paid){
+  constructor(cost, paid, customerAsset, fishAsset){
     this.width = 75
     this.height = 200
     this.x = 500
@@ -7,18 +7,33 @@ class Fish{
     this.cost = cost
     this.paid = paid
     this.change = paid - cost
+    this.customerAsset = loadImage("assets/" + customerAsset)
+    this.fishAsset = loadImage("assets/" + fishAsset)
   }
   
   draw(){
-    //draws fish
-    ellipse(this.x, this.y, this.width, this.height)
+    //draws price
+    push()
+    translate(550,400)
+    rotate(PI/2)
+    image(priceTag, 0, 0, 50, 50)
+    pop()
+
     textSize(20)
     fill("black")
-    text(this.cost, this.x- 15, this.y)
+    text(this.cost, this.x+15, this.y- 130)
     
     //draws change
     textSize(32)
     fill("black")
     text(this.paid, 120, 220)
+
+    //draws fish
+    image(this.fishAsset, 425, 425, 100, 250)
+
+    //draws custoemr
+    image(this.customerAsset, 450, 100, 200, 300)
+
+
   }
 }
--- server/games/game3/sketch.js
+++ server/games/game3/sketch.js
@@ -1,13 +1,8 @@
 function setup() {
   createCanvas(800, 800);
   registerArray = []
-  fishArray = []
-  fish = 0
-  cost = 0
-  change = 0 
-  gameState = 0
-  score = 0
-  level = 4
+  clientAssetArray = ["Boy.png", "Girl.png", "Grandma.png"]
+  fishAssetArray = ["Fish_blue.png", "Fish_green.png", "Fish_orange.png", "Fish_purple.png", "Fish_yellow.png"]
   levelArray = [
     [0.1, 1, 0.1, false],
     [0.1, 1, 0.05, false],
@@ -16,9 +11,13 @@ function setup() {
     [1, 100, 1, true]
   ]
 
+  resetGame()
+
   //loading assets
   registerAsset = loadImage('assets/Register.png')
-  
+
+  priceTag = loadImage('assets/PriceTag.png')
+
   registerArray.push(new Register(104+75*0, 404, 71, 195, 50, "Note50.png", 195, 0, 0))
   registerArray.push(new Register(103+75*1, 404, 71, 195, 20, "Note20.png", 195, 0, 0))
   registerArray.push(new Register(102+75*2, 404, 71, 195, 10, "Note10.png", 195, 0, 0))
@@ -28,9 +27,19 @@ function setup() {
   registerArray.push(new Register(100+75*1, 600, 75, 100, 0.50, "SilverCoin.png", 75, 10, 10))
   registerArray.push(new Register(100+75*2, 600, 75, 100, 0.25, "BrassCoin.png", 75, 10, 10))
   registerArray.push(new Register(100+75*3, 600, 75, 100, 0.10, "BrassCoin.png", 75, 10, 10))
-  
+}
+
+function resetGame(){
+  fishArray = []
+  changeArray = [[],[],[],[],[],[],[],[]]
+  fish = 0
+  cost = 0
+  change = 0 
+  gameState = 0
+  score = 0
+  level = 4
+
   //registerArray.push()
-  
   fishPrice(levelArray[level])
 }
 
@@ -41,6 +50,8 @@ function draw() {
     startPage()
   }else if (gameState == 1){
     gamePage()
+  }else if (gameState == 2){
+    endPage()
   }
 }
 
@@ -52,7 +63,7 @@ function fishPrice(priceArray){
   
   for(i=0; i < 10; i++){
     cost = getRndInteger(startPrice, endPrice, rD)
-    fishArray.push(new Fish(cost, findPaid(cost,endPrice, increment)))
+    fishArray.push(new Fish(cost, findPaid(cost,endPrice, increment), clientAssetArray[getRndInteger(0,3,true)], fishAssetArray[getRndInteger(0,5,true)]))
   }  
 }
 
@@ -70,7 +81,7 @@ function gamePage(){
   textSize(32)
   fill("black")
   text(score, 50, 50)
-  text("Fish", 400, 450)
+  //text("Fish", 400, 450)
   text("Paid", 100, 100)
   text("Change", 250, 100)
   text(change, 300, 150)
@@ -107,6 +118,15 @@ function gamePage(){
   text(".10", 120+75*3, 650)
 }
 
+function endPage(){
+  textSize(32)
+  fill("black")
+  text("End", 370, 350)
+  text(score, 370, 300)
+  noFill()
+  rect(300, 300, 200, 100)
+}
+
 function mouseClicked(){
   if(gameState == 0){
     if((mouseX > 300 && mouseX < 500) && (mouseY > 300 && mouseY < 400)){
@@ -125,6 +145,16 @@ function mouseClicked(){
       }
       change = 0
       fish += 1
+      if(fish > fishArray.length -1){
+        gameState = 2
+      }
+    }
+  }else if (gameState == 2){
+    console.log("endGame1")
+    if((mouseX > 300 && mouseX < 500) && (mouseY > 300 && mouseY < 400)){
+      console.log("end")
+      gameState = 0
+      resetGame()
     }
   }
 }
